b49e5c9b7b60269bb93daa0e2a681ab523dcaced
Daniel Thai <>
2020-08-09 00:37:15 -0400

    Validation logic complete, links made, side draft

--- info.js
+++ info.js
@@ -1,6 +1,12 @@
 window.onload = function(){
 
-    firstnameCheck = function() {
+
+    if (document.querySelector("body").id === "errorpanel") {
+        var statusTitle = document.querySelector("#side").querySelector("h3");
+        var titleText = document.createTextNode("Submission Status");
+        statusTitle.appendChild(titleText);
+    }
+    /*firstnameCheck = function() {
         var fnamefield = document.querySelector("#LastName");
         var fname = fnamefield.getAttribute("value");
 
@@ -12,7 +18,7 @@ window.onload = function(){
         var lname = lnamefield.getAttribute("value");
 
         return nameValid(lname);
-    };
+    };*/
 
     nameValid = function(fieldvalue) {
         var check = /^[A-Z][A-Za-z]{0,5}/;
@@ -20,16 +26,16 @@ window.onload = function(){
         return check.test(fieldvalue);
     };
 
-    passCheck = function() {
+    /*passCheck = function() {
         var passfield = document.querySelector("#Password");
         var passfield2 = document.querySelector("#Re_Password");
 
         return passfield.getAttribute("value") === passfield2.getAttribute("value") && passValid();
-    };
+    };*/
 
-    passValid = function() {
-        var passfield = document.querySelector("#Password");
-        var password = passfield.getAttribute("value");
+    passValid = function(password) {
+        /*var passfield = document.querySelector("#Password");
+        var password = passfield.getAttribute("value");*/
         var bStart = false;
         var rAlpha = /^[A-Za-z]/;
         var bUpper = false;
@@ -54,12 +60,46 @@ window.onload = function(){
         }
     };
 
-    usernameCheck = function() {
-        var userfield = document.querySelector("#Username");
-        var username = userfield.getAttribute("value");
+    usernameCheck = function(username) {
+        /*var userfield = document.querySelector("#Username");
+        var username = userfield.getAttribute("value");*/
         var rStart = /^[A-Za-z]/;
 
         return username.length >= 6 && rStart.test(username);
+    };
+
+    formValidate = function(formdata) {
+        var test = true;
+
+        if (!nameValid(formdata.fName.value) || !nameValid(formdata.lName.value)) {
+            //message
+            test = false;
+        }
+
+        if (formdata.password.value === formdata.re_password.value) {
+            if (!passValid(formdata.password.value))
+                test = false; //invalid password message
+        } else {
+            //non-match message
+            test = false;
+        }
+
+        if (!usernameCheck(formdata.username.value)) {
+            //message
+            test = false;
+        }
+
+        if (formdata.edLvl.value == "0") {
+            //message
+            test = false;
+        }
+
+        if (formdata.gradStatus.value == "none") {
+            //message
+            test = false;
+        }
+
+        return test;
     };
 
 }
